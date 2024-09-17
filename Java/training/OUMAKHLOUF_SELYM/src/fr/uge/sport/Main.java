70ea4d9a1248a3a8857086ecdfc285c3aded9bcb
Selym Oumakhlouf <selym.oumakhlouf@nomachine.univ-eiffel.fr>
2024-09-17 11:51:25 +0200

    Partie 1 sport

--- Java/training/OUMAKHLOUF_SELYM/src/fr/uge/sport/Main.java
+++ Java/training/OUMAKHLOUF_SELYM/src/fr/uge/sport/Main.java
@@ -9,21 +9,45 @@ public class Main {
 	var shirt2 = new Clothing(ClothingType.T_SHIRT,"Burton", 4, 50);
 	var shoePair1 = new ShoePair("Nike", "black", 38, 300);
 	var shoePair2 = new ShoePair("Nike", "white", 44, 300);
+	
+	System.out.println("Polo2 : " + polo2 + "\n");
+	
 	var shop1 = new SportsShop("Italie2");
 	shop1.add(polo);
 	shop1.add(shirt1);
 	shop1.add(shoePair1);
 	shop1.add(shoePair2);
+	
 	var shop2 = new SportsShop("Jaude");
 	shop2.add(shirt2);
 	shop2.add(polo);
 	shop2.add(shoePair1);
 	shop2.add(shoePair1);
 	shop2.add(shoePair2);
+	
+	System.out.println("Shop1 :");
 	System.out.println(shop1);
+	
+	System.out.println("\nShop2 :");
 	System.out.println(shop2);
+	
+	System.out.println("\nQuestion 6 : (shop1 - totalPrice)");
 	System.out.println(shop1.totalPrice());
+	
+	System.out.println("\nQuestion 7 : (shop1 - onSale)");
 	System.out.println(shop1.onSale());
-
+	
+	System.out.println("\nQuestion 8 : (shop2 - shoesBySize)");
+	System.out.println(shop2.shoesBySize());
+	
+	System.out.println("\nQuestion 9 : (shop2 - selectedItems)");
+	System.out.println(shop2.selectedItems(SportsShop::priceTooHigh));
+	
+	System.out.println("\nQuestion 10 : (shop2 - occurences)");
+	System.out.println(shop2.occurrences());
+	
+	System.out.println("\nQuestion 11 : (sameItems)");
+	System.out.println(SportsShop.sameItems(shop1, shop2));
+	
   }
 }
--- Java/training/OUMAKHLOUF_SELYM/src/fr/uge/sport/SportsShop.java
+++ Java/training/OUMAKHLOUF_SELYM/src/fr/uge/sport/SportsShop.java
@@ -5,7 +5,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SportsShop {
   
@@ -35,10 +37,40 @@ public class SportsShop {
 	return Collections.unmodifiableList(onSaleList);
   }
   
-  // à faire
-  public Map<Integer, List<ShoePair>> shoesBySize(){
-	return null;
-	
+    public Map<Integer, List<ShoePair>> shoesBySize(){
+	return articles.stream()
+		.flatMap(a -> switch(a) {
+    	case ShoePair shoePair -> Stream.of(shoePair);
+    	default -> Stream.empty(); // Clothing
+		})
+		.collect(Collectors.groupingBy(ShoePair::size));
+  }
+    
+  static boolean priceTooHigh(Sportswear sportswear) {
+  	return sportswear.price() >= 300;
+  }
+  
+  public List<Sportswear> selectedItems(Predicate<Sportswear> predicate) {
+  	return List.copyOf( 
+  			articles.stream()
+	  			.filter(predicate)
+	  			.toList()
+  			);
+  }
+  
+  public Map<Sportswear, Long> occurrences() {
+  	return articles.stream()
+  			.collect(Collectors.groupingBy(
+  					article -> article, Collectors.counting()
+  					)
+				);
+  }
+  
+  public static boolean sameItems(SportsShop shop1, SportsShop shop2) {
+  	var distinctShop1 = shop1.articles.stream().distinct().toList();
+  	var distinctShop2 = shop2.articles.stream().distinct().toList();
+  	if (distinctShop1.size() != distinctShop2.size()) return false;
+  	return distinctShop1.containsAll(distinctShop2);
   }
   
   @Override
