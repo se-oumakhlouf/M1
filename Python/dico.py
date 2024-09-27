77be75de1c9454047fede9bfb9329f17f3bc2765
Selym Oumakhlouf <selym.oumakhlouf@nomachine.univ-eiffel.fr>
2024-09-27 16:06:38 +0200

    td1 python

---
+++ Python/dico.py
@@ -0,0 +1,11 @@
+import sys
+
+
+f=open(sys.argv[1], encoding="ISO-8859-1")
+donnees=f.read()
+f.close()
+for arg in argv :
+    if string in donnees :
+        print(string + "est dans le dictionnaire")
+    else :
+        print(string + "n'est pas dans le dicitonnaire")
