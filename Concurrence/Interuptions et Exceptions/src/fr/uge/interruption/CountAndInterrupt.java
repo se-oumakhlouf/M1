4d9df160a34ad12ad2174a036e92043ee90863aa
Selym <selymoumakhlouf@gmail.com>
2024-11-18 17:17:18 +0100

    CountAndInterruptions -> ok

--- Concurrence/Interuptions et Exceptions/src/fr/uge/interruption/CountAndInterrupt.java
+++ Concurrence/Interuptions et Exceptions/src/fr/uge/interruption/CountAndInterrupt.java
@@ -3,45 +3,58 @@ package fr.uge.interruption;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CountAndInterrupt {
 
 	public static void main(String[] args) throws InterruptedException, NumberFormatException {
 
-		for (int i = 0; i < 5; i++) {
-			var thread = Thread.ofPlatform().start(() -> {
-				int count = 0;
+		Map<Long, Thread> threads = new HashMap<>();
 
-				for (;;) {
+		for (int i = 0; i < 4; i++) {
+			Thread thread = Thread.ofPlatform().start(() -> {
+				long id = Thread.currentThread().threadId();
+				int count = 0;
+				for (; !Thread.currentThread().isInterrupted();) {
 					count++;
 					try {
 						Thread.sleep(1_000);
 					} catch (InterruptedException e) {
+						System.out.println("Thread " + id + " interrupted");
 						return;
 					}
-					System.out.println(Thread.currentThread().threadId() + " counter = " + count);
+					System.out.println("Thread " + id + " counter = " + count);
 				}
-				
 			});
+			threads.put(thread.threadId(), thread);
+		}
+
+		System.out.println("enter a thread id:");
+		try (var input = new InputStreamReader(System.in); var reader = new BufferedReader(input)) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				try {
+					long threadId = Long.parseLong(line);
+					Thread thread = threads.get(threadId);
+					if (thread != null) {
+						thread.interrupt();
+						threads.remove(threadId);
+					} else {
+						System.out.println("No thread with ID : " + threadId);
+					}
+				} catch (NumberFormatException e) {
+					System.out.println("Enter a valid thread ID");
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		
-	  System.out.println("enter a thread id:");
-	  try (var input = new InputStreamReader(System.in);
-	         var reader = new BufferedReader(input)) {
-	      String line;
-	      while ((line = reader.readLine()) != null) {
-	        var threadId = Integer.parseInt(line);
-//	        System.out.println("Thread id : " + threadId);
-//	        System.out.println(Thread.currentThread().getName()); // thread main
-	        if (Thread.currentThread().threadId() == threadId) {
-	        	System.out.println("equal");
-	        	Thread.currentThread().interrupt();
-	        }
-	      }
-	    } catch (IOException e) {
-	    	System.out.println("here");
-				return;
-			}
-
+		System.out.println("Exiting -> Interrupting all threads");
+		for (var thread : threads.values()) {
+			thread.interrupt();
+		}
+		
 	}
 }
