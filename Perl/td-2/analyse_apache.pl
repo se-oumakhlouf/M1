7e7a1e7c73a698793f1ef9b94140e384fe4bd935
se-oumakhlouf.wsl2 <selymoumakhlouf@gmail.com>
2025-01-17 19:51:54 +0100

    finit

--- Perl/td-2/analyse_apache.pl
+++ Perl/td-2/analyse_apache.pl
@@ -9,7 +9,9 @@ use warnings;
 my $nb_ligne = 0;
 my $nb_errors = 0; # statut autre que 200;
 my $sum_volume = 0;
-my %access = ();
+my %access_url = ();
+my %access_ip = ();
+my %ip_volume = ();
 
 open (my $fd , '<', 'access_log.txt') or die ("open: $!");
 while (defined (my $ligne = <$fd>)) {
@@ -18,21 +20,33 @@ while (defined (my $ligne = <$fd>)) {
 
     if (my ($ip, $url, $status, $volume) = $ligne =~  m/^(.*?) .*?".*? (.*?) .*?" (\d{3}) (.*?) /) {
         $sum_volume += $volume;
-        $access{$url}++;
+        $access_url{$url}++;
+        $access_ip{$ip}++;
+        $ip_volume{$ip} += $volume;
         if ($status != 200) {
             $nb_errors++;
         }
     }
 }
 close($fd);
-print "Nombre de lignes : $nb_ligne\n";
+print "\nNombre de lignes : $nb_ligne\n";
 print "Nombre d'erreurs : $nb_errors\n";
 print "Nombre total d'octets transférés : $sum_volume\n";
 
-my $index = 0;
-print "Urls dans l'ordre decroissant du nombre d'accès : \n";
-foreach my $url ( sort { $access{$b} <=> $access{$a} } keys %access) {
-    print "$url -> $access{$url}\n";
-    $index++;
-    last if $index == 4;
+my $index_url = 0;
+my $limit_url = 5;
+print "\nUrls dans l'ordre decroissant du nombre d'accès (limit = $limit_url) : \n";
+foreach my $url ( sort { $access_url{$b} <=> $access_url{$a} } keys %access_url) {
+    print "$url : access=$access_url{$url}\n";
+    $index_url++;
+    last if $index_url == $limit_url;
+}
+
+my $index_ip = 0;
+my $limit_ip = 10;
+print "\nIP ayant accédé le plus de fois au serveur (limit = $limit_ip) :\n";
+foreach my $ip ( sort { $access_ip{$b} <=> $access_ip{$a} } keys %access_ip) {
+    print "$ip : access=$access_ip{$ip} volume=$ip_volume{$ip}\n";
+    $index_ip++;
+    last if $index_ip == $limit_ip;
 }
--- Perl/td-2/regex.pl
+++ Perl/td-2/regex.pl
@@ -3,8 +3,9 @@
 use strict;
 use warnings;
 
-
+my $index = 0;
 open (my $fd , '<', 'passwd.txt') or die ("open: $!");
+
 while (defined (my $ligne = <$fd>)) {
     chomp $ligne;
 
@@ -28,8 +29,20 @@ while (defined (my $ligne = <$fd>)) {
 
 
     # Question 5 : swap login et passwd
-    $ligne =~ s/^(.*?):(.*?):/$2:$1/;
+    # $ligne =~ s/^(.*?):(.*?):/$2:$1/;
+
+    # Question 6 : swap uid et gid
+    # $ligne =~ s/^((?:.*?:){2})(.*?):(.*?):/$1:$3:$2:/;
+
+    # Question 7 : affichez uniquement le gid
+    # if (my ($gid) = $ligne =~ m/^(?:.*?:){3}(.*?):/) {
+    #     print "$gid\n";
+    # }
+
+    # Question 8 :  affichez les lignes en multipliant le gid par 2 (option e)
+    $ligne =~ s/^((?:.*?:){3})(.*?)(:)/$1 . $2 * 2 . $3/e;
 
     print "$ligne\n";
 }
+
 close($fd);
