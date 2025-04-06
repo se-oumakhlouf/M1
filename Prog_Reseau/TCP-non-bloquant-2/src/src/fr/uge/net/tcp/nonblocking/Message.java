ea0af0b9aae5a3f8f6ab9a279c9ba6bfaf13e0de
se-oumakhlouf.wsl2 <selymoumakhlouf@gmail.com>
2025-04-06 19:57:24 +0200

    clean

--- Prog_Reseau/TCP-non-bloquant-2/src/src/fr/uge/net/tcp/nonblocking/Message.java
+++
@@ -1,8 +0,0 @@
-package fr.uge.net.tcp.nonblocking;
-
-public record Message(String login, String text) {
-    @Override
-    public String toString() {
-        return login + ": " + text;
-    }
-}
